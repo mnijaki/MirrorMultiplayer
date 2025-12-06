36c35013adc3a189fc9f69a7539e26739d4c981e
MarekNijaki <mnijaki@gmail.com>
2025-12-06 11:03:40 +0100

    * Added TextMeshPro package * Added handling for displaying color to CustomNetworkPlayer * Added handling for displaying name to CustomNetworkPlayer * Moved camer so it displays player * Added UI elements to player prefab * Hooked up references in player prefab

--- Assets/Scripts/CustomNetworkPlayer.cs
+++ Assets/Scripts/CustomNetworkPlayer.cs
@@ -1,15 +1,24 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class CustomNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar]
+    [SyncVar(hook = nameof(OnDisplayNameChanged))]
     [SerializeField]
     private string _displayName = "Missing Player name.";
     
-    [SyncVar]
+    [SyncVar(hook = nameof(OnColorChanged))]
     [SerializeField]
     private Color _color = Color.white;
+    
+    [SerializeField]
+    private TMP_Text _displayNameText;
+    
+    [SerializeField]
+    private MeshRenderer _playerMeshRenderer;
+    
+    private static readonly int _baseColorID = Shader.PropertyToID("_BaseColor");
 
     [Server]
     public void SetDisplayName(string displayName)
@@ -27,5 +36,17 @@ public class CustomNetworkPlayer : NetworkBehaviour
     public void SetRandomColor()
     {
         SetColor(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+    }
+
+    [Client]
+    private void OnDisplayNameChanged(string oldDisplayName, string newDisplayName)
+    {
+        _displayNameText.SetText(newDisplayName);
+    }
+    
+    [Client]
+    private void OnColorChanged(Color oldColor, Color newColor)
+    {
+        _playerMeshRenderer.material.SetColor(_baseColorID, newColor); 
     }
 }
